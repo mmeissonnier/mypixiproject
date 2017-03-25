69a9090e3ddc09fb985243ca28804fc75c1ad6bd
Mathieu MEISSONNIER <mmeissonnier@gmail.com>
2017-03-25 17:31:33 +0100

    02-Displaying Sprites

--- src/index.js
+++ src/index.js
@@ -1,52 +1,22 @@
 import Scene from './Scene';
-import KeyboardManager from './KeyboardManager';
-import Bump from './utils/bump.js';
 import Dragon from './Dragon.js';
 import AnimatedDragon from './AnimatedDragon.js';
 
-
 const scene = new Scene('container');
-const keyboard = new KeyboardManager();
-const bump = new Bump(PIXI);
-
-const filter = new PIXI.filters.ColorMatrixFilter();
 
 //SETUP
 const setup = () => {
   const dragon = new Dragon();
-  dragon.filters = [filter];
   scene.addChild(dragon, 'dragon1');
 
   const dragon2 = new AnimatedDragon();
   dragon2.position.set(300, 300);
   dragon2.play();
-  dragon2.filters = [filter];
 
-  scene.addChild(dragon2, 'dragon2');
+//  scene.addChild(dragon2, 'dragon2');
 
   //START RENDERING
   scene.render();
-
-  //LISTEN TO KEYBOARD
-  keyboard.start();
-
-
-  // DEFINE TASKS
-  const collisionDetectionTask = () => {
-    if (bump.hitTestRectangle(scene.getChildById('dragon1'), scene.getChildById('dragon2'))) {
-      filter.negative();
-    } else {
-      filter.reset();
-    }
-  }
-
-  const moveDragonTask = () => {
-    const dragon = scene.getChildById('dragon2');
-    dragon.position.set(dragon.x + keyboard.translation.x, dragon.y + keyboard.translation.y);
-  }
-
-  scene.renderTasks.concat([collisionDetectionTask,
-                            moveDragonTask]);
 };
 
 
